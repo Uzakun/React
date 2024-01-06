20348b10d274a0539e863a5aa65387d0dddc4426
Shwetank Chandra <96305925+Uzakun@users.noreply.github.com>
2024-01-06 16:10:21 +0530

    upgrade TextUtils

--- TextUtils/App.js
+++ TextUtils/App.js
@@ -1,15 +1,50 @@
 import './App.css';
+// import DarkMode from './Components/DarkMode';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
- 
+import React, { useState } from 'react';
+import Alert from './Components/Alert';
+
+
+
 function App() {
+  const [mode, setMode] = useState('light');
+
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  }
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = "#666666";
+      showAlert("Dark mode Enabled", "success")
+    }
+    else {
+      setMode('light')
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode Enabled", "success")
+    }
+  }
+
   return (
     <>
-    <Navbar title="TextUtils" />
-    <div className="container my-3">
-    <TextForm heading="Enter The Text Below"/>
-    </div>
-    </> 
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+      <Alert alert={alert} />
+      <div className="container my-3">
+        <TextForm heading="Enter The Text Below" mode={mode} showAlert={showAlert} />
+        {/* <DarkMode/> */}
+      </div>
+
+    </>
   );
 }
 
