54d36660eda7c9eb7e5f5024df5e0caba2af9fba
Shwetank Chandra <96305925+Uzakun@users.noreply.github.com>
2024-01-06 16:11:34 +0530

    upgrade textUtils add more components and code

--- TextUtils/Components/TextForm.js
+++ TextUtils/Components/TextForm.js
@@ -3,41 +3,44 @@ import React, { useState } from 'react'
 
 export default function TextForm(props) {
     const handleUpClick = () => {
-        let newText = text.toUpperCase();
-        setText(newText)
+        let newUpText = text.toUpperCase();
+        setText(newUpText);
+        props.showAlert("Upper Case Enabled", "success");
+
     }
 
     const handleLoClick = () => {
-        let newText = text.toLowerCase();
-        setText(newText)
+        let newLoText = text.toLowerCase();
+        setText(newLoText);
+        props.showAlert("Lower Case Enabled", "success");
     }
 
 
     const handleOnChange = (event) => {
-        setText(event.target.value)
+        setText(event.target.value);
     }
 
-    const [text, setText] = useState('Enter text here');
+    const [text, setText] = useState("");
     // text = "new text"; // Wrong way to change the state
     // setText("new text"); // Correct way to change the state
     return (
         <>
             <div>
-                <h1>{props.heading}</h1>
+                <h1 style={{color: props.mode==='dark'?'white':'black'}}>{props.heading}</h1>
                 <div className="mb-3">
-                    <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                    <textarea className="form-control" value={text} onChange={handleOnChange}  style={{backgroundColor: props.mode==='dark'?'#606060':'white', color: props.mode==='dark'?'white':'black'}} id="myBox" rows="8"></textarea>
                 </div>
                 <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
                 <button className="btn btn-primary mx-3" onClick={handleLoClick}>Convert to Uppercase</button>
             </div>
-            <div className="container my-3">
+            <div className="container my-3" style={{color: props.mode==='dark'?'white':'black'}}>
                 <h1>Words and Characters</h1>
-                <p>{text.split(" ").length} words and {text.length} Characters</p>
+                <p>{text.split().length} words and {text.length} Characters</p>
                 <p>{0.008 * text.split(" ").length} time(in Minutes) will be taken to read this whole text.</p>
           </div>
-          <div className="container my-3">
+          <div className="container my-3" style={{color: props.mode==='dark'?'white':'black'}}>
             <h2>Preview</h2>
-                <p>{text}</p>
+                <p>{text.length>0?text:"Enter something in the above box to preview it here."}</p>
           </div>
         </>
     )
