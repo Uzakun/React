26ff5407dc99b995150ebb30083096aca8fbcbe9
Shwetank Chandra <96305925+Uzakun@users.noreply.github.com>
2024-01-07 18:30:25 +0530

    Learn Router

--- TextUtils/App.js
+++ TextUtils/App.js
@@ -1,9 +1,15 @@
 import './App.css';
-// import DarkMode from './Components/DarkMode';
+import About from './Components/About';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
 import React, { useState } from 'react';
 import Alert from './Components/Alert';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  
+} from "react-router-dom";
 
 
 
@@ -27,25 +33,43 @@ function App() {
       setMode('dark');
       document.body.style.backgroundColor = "#666666";
       showAlert("Dark mode Enabled", "success")
+      document.title = "In Dark Mode";
     }
     else {
       setMode('light')
       document.body.style.backgroundColor = "white";
       showAlert("Light mode Enabled", "success")
+      document.title = "In light Mode";
     }
   }
 
   return (
     <>
-      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
-      <Alert alert={alert} />
-      <div className="container my-3">
-        <TextForm heading="Enter The Text Below" mode={mode} showAlert={showAlert} />
-        {/* <DarkMode/> */}
-      </div>
+      <Router>
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        
+        <div className="container my-3">
 
+          <Switch>
+            <Route exact path="/about">
+              <About />
+            </Route>
+            <Route exact path="/">
+              <TextForm heading="Enter The Text Below" mode={mode} showAlert={showAlert} />
+            </Route>
+
+
+          </Switch>
+
+
+        </div>
+      </Router>
     </>
   );
 }
 
 export default App;
+
+
+
